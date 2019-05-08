1821038d631bd78381c44f6f38ffb4ff9da62308
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-05-08 21:26:55 +0200

    ShoppingList App - useState Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const addItemToShoppingList = () => {
+    setShoppingList([...shoppingList, "Marchewka"]);
+  };
+
   return (
-    <div className="App">Start</div>
+    <div className="App">
+      <ul>
+        {shoppingList.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <input type="text" name="item" />
+      <button onClick={addItemToShoppingList}>
+        Add
+      </button>
+    </div>
   );
 }
 
