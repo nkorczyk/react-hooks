e2750daf8f9867f48bcb95752f06dffb04ee5568
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-05-08 22:03:54 +0200

    CountriesFiltering App - finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { countries } from './countries';
 import './App.css';
 
 function App() {
+  const [countryList, setCountryList] = useState(countries);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = event => {
+    setInputValue(event.target.value);
+    setCountryList(countryList
+      .filter(country => country.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase())));
+
+    if (!event.target.value.length) setCountryList(countries);
+  };
+
   return (
-    <div className="App">Start</div>
+    <div className="App">
+      <label for="country">Search Country</label>
+      <input
+        id="country"
+        type="text"
+        name="country"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <ul>
+        {countryList.map(country => (
+          <li>{country}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
