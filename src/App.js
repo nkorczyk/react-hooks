00a812c3034f27b1811a9143c16ebe32b0697d1b
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-05-08 21:08:13 +0200

    Counter App - useState Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, changeCounter] = useState(0); // returns => [state, setState]
+
   return (
-    <div className="App">Start</div>
+    <div className="App">
+      <h1>{counter}</h1>
+      <button
+        onClick={() => changeCounter(counter - 1)}>
+        -
+        </button>
+      <button
+        onClick={() => changeCounter(counter + 1)}>
+        +
+      </button>
+    </div>
   );
 }
 
